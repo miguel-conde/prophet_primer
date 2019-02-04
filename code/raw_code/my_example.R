6de3a530cdb4d23da05cdee3c0b0c60e2583156e
miguel-conde <miguelco2000@gmail.com>
2019-02-04 22:04:15 +0100

    CORES model

--- code/raw_code/my_example.R
+++ code/raw_code/my_example.R
@@ -267,3 +267,105 @@ m_aportes <- forecast %>% as.tibble() %>%
 
 Conento::aportes_ag(m_aportes)
 Conento::dibuja_areas(m_aportes %>% mutate(ds = as.Date(ds)))
+
+
+# CORES -------------------------------------------------------------------
+
+library(tidyverse)
+library(lubridate)
+library(prophet)
+library(readxl)
+
+URL_CORES <- "https://www.cores.es/sites/default/files/archivos/estadisticas/consumos-pp.xlsx"
+
+get_cores_data <- function(sheet = "Gasolinas") {
+  #
+  # Esta función se baja de la web de CORES el archivo excel con los consumos 
+  # nacionales mensuales de productos petrolíferos
+  #
+  # PARAMETERS
+  #   - sheet: char("Gasolinas", "Gasoleos") - hoja del libro excel cuyos datos 
+  #            se desea obtener
+  #
+  # RETURN
+  #   - Tibble con <fecha> (ajustada al dia fina de mes) y una columna para el 
+  #     consumo mensual (en Tn) por producto 
+  
+  destfile <- paste0(tempfile(), ".xlsx")
+  
+  curl::curl_download(URL_CORES, destfile)
+  
+  consumos_pp <- read_excel(destfile, sheet = sheet, skip = 5) %>% 
+    janitor::clean_names() %>% 
+    filter(!is.na(mes)) %>% 
+    filter(mes != "total") %>% 
+    mutate(dia = 1) %>% 
+    unite(fecha, c("ano", "mes", "dia"), sep = "-") %>% 
+    mutate(fecha = ymd(fecha))
+  
+  unlink(destfile)
+  
+  day(consumos_pp$fecha)  <-  days_in_month(consumos_pp$fecha)
+  
+  consumos_pp
+}
+
+tb_gna95 <- get_cores_data() %>% 
+  select(fecha, gasolina_95) %>% 
+  rename(ds = fecha,
+         y = gasolina_95)
+
+tb_goa   <- get_cores_data("Gasoleos") %>% 
+  select(fecha, gasoleo_a) %>% 
+  rename(ds = fecha,
+         y = gasoleo_a)
+
+# Fit the model
+m <- prophet(tb_goa)
+
+# Make predictions
+future <- make_future_dataframe(m, periods = 24, freq = 'month')
+tail(future)
+
+forecast <- predict(m, future)
+tail(forecast[c('ds', 'yhat', 'yhat_lower', 'yhat_upper')])
+
+# Plots
+plot(m, forecast)
+
+prophet_plot_components(m, forecast)
+
+dyplot.prophet(m, forecast)
+
+
+
+### PERF
+tb_data_cv <- cross_validation(m, 
+                               initial = 365.25*4, 
+                               period = 365.25, 
+                               horizon = 365.25*2, 
+                               units = 'days')
+head(tb_data_cv)
+tb_data_cv %>% select(cutoff) %>% unique
+tb_data_cv %>% group_by(cutoff) %>% summarise(n = n()) 
+
+tb_data_p <- performance_metrics(tb_data_cv)
+head(tb_data_p)
+
+tb_data_p_by_h <- tb_data_p %>% 
+  group_by(horizon) %>% 
+  summarise_all(mean)
+
+with(tb_data_p_by_h,
+     plot(horizon, mse, type = "l"))
+with(tb_data_p_by_h,
+     plot(horizon, rmse, type = "l"))
+with(tb_data_p_by_h,
+     plot(horizon, mae, type = "l"))
+with(tb_data_p_by_h,
+     plot(horizon, mape, type = "l"))
+with(tb_data_p_by_h,
+     plot(horizon, coverage, type = "l"))
+
+plot_cross_validation_metric(tb_data_cv, metric = 'mape')
+
